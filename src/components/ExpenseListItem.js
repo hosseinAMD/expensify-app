0c1500436df547f66f89ad1846357baa456160f4
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-01-08 13:08:09 +0330

    Reformat amount and createdAt for expenses itmes

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-
+import moment from 'moment';
 
 const ExpenseListItem = (props) => (
     <div>
         <Link to={`/edit/${props.expense.id}`}><h3>{props.expense.description}</h3></Link>
-        <p>{props.expense.amount}--{props.expense.createdAt}</p>
+        <p>{`${props.expense.amount} Rials`}
+        --
+        {moment(props.expense.createdAt).format('MMMM Do, YYYY')}</p>
     </div>
 );
 
